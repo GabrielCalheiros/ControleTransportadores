9b72eb24e15ba6164f5d956acd76572628bd7762
GabrielCalheiros <gabrielbcm.1998@gmail.com>
2023-03-14 15:54:47 -0300

    Create a new controller using the command php artisan make:controller EmpresaController, and define a store method that receives the form data and saves it to the database

---
+++ FornecControl/app/Http/Controllers/EmpresaController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\empresa;
+
+class empresaController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'endereco' => 'required|string|max:255',
+            'nome_fantasia' => 'required|string|max:255',
+            'cnpj' => 'required|string|unique:empresas,cnpj|max:14',
+        ]);
+
+        $empresa = new empresa;
+        $empresa->endereco = $validatedData['endereco'];
+        $empresa->nome_fantasia = $validatedData['nome_fantasia'];
+        $empresa->cnpj = $validatedData['cnpj'];
+        $empresa->save();
+
+        return redirect()->back()->with('success', 'empresa cadastrada com sucesso!');
+    }
+}
